1695207b75104f0a546e28bde90ad2c482a55c4d
Hidehiro Nagaoka <kuinige@gmail.com>
2017-05-31 14:41:19 +0900

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :request
   belongs_to :author, polymorphic: true
+
+  validates :content, presence: true
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :content, presence: true
 end
