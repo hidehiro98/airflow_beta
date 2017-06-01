5c3ef04be8c78a91c6cf18cb90792705ad67d7a5
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-01 16:01:49 +0900

    add enum to Request model and Receiver model

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -2,4 +2,6 @@ class Receiver < ApplicationRecord
   belongs_to :user
   belongs_to :request
   has_many :comments, as: :author
+
+  enum status: { default: 0, accepted: 1, rejected: 2, commented: 3 }
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  enum status: { default: 0, accepted: 1, rejected: 2, commented: 3 }
 end
