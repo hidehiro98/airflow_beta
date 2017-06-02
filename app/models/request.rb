99ab5370cb89025ec830dbc56923a44d6d6398b6
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-02 10:11:16 +0900

    change status name from default to pending

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,5 @@ class Request < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  enum status: { default: 0, accepted: 1, rejected: 2, commented: 3 }
+  enum status: { pending: 0, accepted: 1, rejected: 2, commented: 3 }
 end
