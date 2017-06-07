7ecae7611008c1369be1c34f9bfc1de89fc6932b
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-07 10:46:54 +0900

    change seeds file and delete more button from show

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,10 +22,11 @@ u2 = User.create!(email: 'a@c.com', password: '123123', team: t, first_name: 'Di
 
 puts 'Creating requests...'
 r1 = Request.create!(content: 'Buying new PC', user: u1, duedate: Time.zone.now + 5.days)
-r2 = Request.create!(content: 'Cealing company stamp', user: u1, duedate: Time.zone.now + 5.days)
+r2 = Request.create!(content: 'Cealing company stamp', user: u2, duedate: Time.zone.now + 5.days)
 
 puts 'Creating receivers...'
 rc1 = Receiver.create!(user: u2, request: r1)
+rc2 = Receiver.create!(user: u1, request: r2)
 
 puts 'Creating comments...'
 Comment.create!(content: 'Greato job!', request: r1, author: rc1)
