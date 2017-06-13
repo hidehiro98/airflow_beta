69f8afd5f8bf0baee451040091e81e0541c59c8b
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-13 13:11:41 +0900

    add reject function to the receiverd cards

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -5,5 +5,7 @@ class ReceiversController < ApplicationController
   end
 
   def reject
+    current_user.receivers.find_by(request_id: params[:id]).rejected!
+    redirect_back(fallback_location: root_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
     member do
       patch 'accept', to: 'receivers#accept'
+      patch 'reject', to: 'receivers#reject'
     end
   end
 
