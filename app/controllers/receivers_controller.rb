ba6207cd0760e13832ef58e2562a85cd0cbdb581
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-14 13:46:49 +0900

    create check status method of request

--- app/controllers/receivers_controller.rb
+++ app/controllers/receivers_controller.rb
@@ -1,6 +1,7 @@
 class ReceiversController < ApplicationController
   def accept
     current_user.receivers.find_by(request_id: params[:id]).accepted!
+    Request.find(params[:id]).check_status
     redirect_back(fallback_location: root_path)
   end
 
--- app/models/request.rb
+++ app/models/request.rb
@@ -7,4 +7,20 @@ class Request < ApplicationRecord
   validates :content, :user, :duedate, presence: true
 
   enum status: { pending: 0, accepted: 1, rejected: 2, commented: 3 }
+
+  def check_status
+    check = false
+
+    receivers.each do |receiver|
+      if receiver.status == 'accepted'
+        check = true
+      else
+        check = false
+        break
+      end
+    end
+
+    accepted! if check
+  end
+
 end
