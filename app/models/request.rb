4f10b4dddfdb8bca655719a46be50a8d5a764f02
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-16 13:57:24 +0900

    update check status method

--- app/models/request.rb
+++ app/models/request.rb
@@ -9,18 +9,7 @@ class Request < ApplicationRecord
   enum status: { pending: 0, accepted: 1, rejected: 2, commented: 3 }
 
   def check_status
-    check = false
-
-    receivers.each do |receiver|
-      if receiver.status == 'accepted'
-        check = true
-      else
-        check = false
-        break
-      end
-    end
-
-    accepted! if check
+    accepted! if receivers.all?(&:accepted?)
   end
 
 end
