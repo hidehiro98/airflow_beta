6f300c2167a0d73eeb2b0bd6394502060f145ded
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-23 14:22:12 +0900

    create show page of profile

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
+  resource :profile, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
