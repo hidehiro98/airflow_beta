1ac8e86486b9b66f98c347d7006163c6763c9cef
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-26 12:29:10 +0900

    update photo uploader display the only face in good size

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,13 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :bright_face do
+    cloudinary_transformation width: 48, height: 48,
+      crop: :thumb, gravity: :face
+  end
+
 end
