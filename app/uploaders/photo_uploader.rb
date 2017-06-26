fbc9fe6d65122470b09c686ead03600775c3877a
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-26 13:28:57 +0900

    add profile photo in navbar

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,8 +5,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   process convert: 'jpg'
 
-  version :bright_face do
+  version :profile_face do
     cloudinary_transformation width: 48, height: 48,
+      crop: :thumb, gravity: :face
+  end
+
+  version :navbar_face do
+    cloudinary_transformation width: 40, height: 40,
       crop: :thumb, gravity: :face
   end
 
