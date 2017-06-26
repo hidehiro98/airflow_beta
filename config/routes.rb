eb9b8f401d71c1f8fa87ea6ef7b4efa95e0a819a
Hidehiro Nagaoka <kuinige@gmail.com>
2017-06-26 19:04:53 +0900

    add cancel function

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     member do
       patch 'accept', to: 'receivers#accept'
       patch 'reject', to: 'receivers#reject'
+      patch 'cancel', to: 'requests#cancel'
     end
   end
 
