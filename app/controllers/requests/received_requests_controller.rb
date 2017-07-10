6ccc340d255acd46413dbaee219646e8c3f764f7
Hidehiro Nagaoka <kuinige@gmail.com>
2017-07-10 14:54:07 +0900

    fix received requests controller's bug

--- app/controllers/requests/received_requests_controller.rb
+++ app/controllers/requests/received_requests_controller.rb
@@ -1,9 +1,9 @@
 class Requests::ReceivedRequestsController < ApplicationController
   def index
-    @requests = current_user.receivers.pending.map(&:request).sort(&:duedate)
+    @requests = current_user.receivers.pending.map(&:request).sort_by(&:duedate)
   end
 
   def replied
-    @requests = current_user.receivers.replied.map(&:request).sort(&:duedate)
+    @requests = current_user.receivers.replied.map(&:request).sort_by(&:duedate)
   end
 end
