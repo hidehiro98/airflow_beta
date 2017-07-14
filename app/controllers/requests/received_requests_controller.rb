55c5d6058fb77b1a0f1bca978835d285c20b580f
Hidehiro Nagaoka <kuinige@gmail.com>
2017-07-14 20:06:31 +0900

    update received requests controller not to use ruby but to use active record

--- app/controllers/requests/received_requests_controller.rb
+++ app/controllers/requests/received_requests_controller.rb
@@ -1,9 +1,13 @@
 class Requests::ReceivedRequestsController < ApplicationController
   def index
-    @requests = policy_scope(Receiver).pending.map(&:request).sort_by(&:duedate)
+    receivers = policy_scope(Receiver).pending
+    @requests = Request.where(id: receivers.pluck(:request_id))
+    # @requests = policy_scope(Receiver).pending.map(&:request).sort_by(&:duedate)
   end
 
   def replied
-    @requests = policy_scope(Receiver).replied.map(&:request).sort_by(&:duedate)
+    receivers = policy_scope(Receiver).replied
+    @requests = Request.where(id: receivers.pluck(:request_id))
+    # @requests = policy_scope(Receiver).replied.map(&:request).sort_by(&:duedate)
   end
 end
